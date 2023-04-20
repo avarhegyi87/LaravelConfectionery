13a224021247ede25be9b350f63db327fcdbd3bc
csabaialbert <csabaialbert@gmail.com>
2023-04-20 18:30:28 +0200

    debugged-feature-listByTypes

---
+++ resources/views/components/free-card.blade.php
@@ -0,0 +1,26 @@
+@props(['confection'])
+<?php $prize = 'no';
+if ($confection->prizewinning == 1)
+    $prize='yes';
+
+$freeFrom = 'It is free from:';
+foreach ($confection->contents as $value)
+{
+    $freeFrom = $freeFrom. ' ' .$value->free;
+}
+?>
+<x-card>
+    
+    <div class="flex">
+        
+        <div>
+            <h3 class="text-2xl">
+                <a href="/database/{{$confection->id}}">{{$confection->cname}}</a>
+            </h3>
+            <div class="text-xl mb-4">Is it prizewinning: {{$prize}}.</div>
+            <div class="text-lg mt-4"> Type: {{$confection->type}}</div>
+            <div class="text-lg mt-4"> {{$freeFrom}}</div>
+
+        </div>
+    </div>
+</x-card>
--- resources/views/database/show.blade.php
+++ resources/views/database/show.blade.php
@@ -20,11 +20,6 @@
         <a href="/database/{{$confection->id}}/edit">
             <i class="fa-solid fa-pencil"></i> Edit
         </a>
-        <form method="POST" action="/confections/{{$confection->id}}">
-        @csrf
-        @method('DELETE')
-        <button class="text-red-500"> <i class="fa-solid fa-trash"></i> Delete</button>
-        </form>
     </x-card>
 
 @endsection
