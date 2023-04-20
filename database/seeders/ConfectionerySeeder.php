4ed280973fe235d0094457c6a1b5ec10dee3a2ed
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-04-20 23:22:42 +0200

    feat: ConfectionerySeeder

---
+++ database/seeders/ConfectionerySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ConfectionerySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tables = ['confections', 'contents', 'prices'];
+        foreach ($tables as $table) {
+            $this->seedFileToTable(storage_path($table . '.txt'), $table);
+        }
+    }
+
+    private function seedFileToTable($filePath, $tableName)
+    {
+        $contents = file($filePath);
+
+        foreach ($contents as $content) {
+            $values = explode("\t", $content);
+
+            if (!isset($columns)) {
+                $columns = array_map(function ($value) {
+                    return str_replace(["\r", "\n"], '', $value);
+                }, $values);
+            } else {
+                $data = array_combine($columns, $values);
+                DB::table($tableName)->insert($data);
+            }
+        }
+    }
+}
