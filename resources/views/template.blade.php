e540e661f6fa676f649c637af50cc9e3e9a82c67
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-04-23 12:42:02 +0200

    fix: logo location, remove bootstrap import

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -38,12 +38,9 @@
     @include('_includes.nav.topnav')
     <div class="mb-24">
     @yield('content')
-    
+
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
         integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
-    </script>
 
     </div>
     <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
